5e42803d8672621869e2dc70d8ade90e4e87a905
juanraba <jrabanillog@gmail.com>
2018-07-04 11:24:44 -0400

    add 3/ejer1

--- 3 strings/ejercicio1.rb
+++ 3 strings/ejercicio1.rb
@@ -1,7 +1,15 @@
-# Dado el siguiente string y caracter, crear un método que reciba como parámetro el string
+puts '# Dado el siguiente string y caracter,
+crear un método que reciba como parámetro el string
 # y el caracter. Luego debe buscar si existe ese caracter dentro del string.
 # hint: El método .include? de un string busca si un caracter
-# o string dado está contenido en éste.
+# o string dado está contenido en éste.'
+
+def include?(str, chr)
+  str.include?(chr)
+end
 
 cadena = 'Hola Mundo!'
 caracter = 'o'
+caracter2 = 'x'
+puts include?(cadena, caracter)
+puts include?(cadena, caracter2)
