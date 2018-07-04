e930f13ba2784ecaa06f64fb81a67adbd2c6e701
juanraba <jrabanillog@gmail.com>
2018-07-04 11:58:03 -0400

    add 3/ejer2

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -1,4 +1,36 @@
-# Construir un arreglo de los nombres de todos sus compañeros y en base a él:
-# 1. Imprimir todos los elementos que excedan más de 5 caracteres.
-# 2. Crear un arreglo nuevo con todos los elementos en minúscula.
-# 3. Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
+puts '# Construir un arreglo de los nombres
+de todos sus compañeros y en base a él:'
+nombres = %w[
+  Dani Jorge Juan Mai Patricio Stu Tomás
+  Leudis Leonardo Marcelo Gabriela Damian
+]
+def show(array)
+  print array
+  puts ''
+end
+
+show nombres
+
+puts '# 1. Imprimir todos los elementos que excedan más de 5 caracteres.'
+nombres.each do |elem|
+  print "#{elem} " if elem.size > 5
+end
+
+puts '# 2. Crear un arreglo nuevo con todos los elementos en minúscula.'
+nombres_min = []
+nombres.each do |elem|
+  nombres_min.push(elem.downcase)
+end
+show nombres_min
+
+puts '# 3. Crear un método que devuelva un arreglo con la cantidad de
+ caracteres que tiene cada nombre.'
+def str_len(array)
+  arr_len = []
+  array.each do |elem|
+    arr_len.push(elem.size)
+  end
+  arr_len
+end
+
+show str_len(nombres)
