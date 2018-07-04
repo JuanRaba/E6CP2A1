49c96834705ee099db3576d3a21139bafb64ab58
juanraba <jrabanillog@gmail.com>
2018-07-03 20:30:03 -0400

    add 2/ejer1

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -1,8 +1,30 @@
-# Dado el array:
-# 1. Mostrar en pantalla el primer elemento.
-# 2. Mostrar en pantalla el último elemento.
-# 3. Mostrar en pantalla todos los elementos.
-# 4. Mostrar en pantalla todos los elementos junto con un índice.
-# 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
+puts 'Dado el array:'
+arreglo = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+print arreglo
 
-arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+puts '1. Mostrar en pantalla el primer elemento.'
+puts arreglo.first
+
+puts '2. Mostrar en pantalla el ultimo elemento.'
+puts arreglo.last
+
+text_mostrar = 'Mostrar en pantalla todos los elementos'
+puts "3. #{text_mostrar}."
+print arreglo
+puts ''
+
+puts "# 4. #{text_mostrar} junto con un indice."
+i = 0
+arreglo.each do |elem|
+  print "#{i}:#{elem} "
+  i += 1
+end
+puts ''
+
+puts "# 5. #{text_mostrar} que se encuentren en una posicion par."
+i = 0
+arreglo.each do |elem|
+  print "#{i}:#{elem} " if i.even?
+  i += 1
+end
+puts ''
